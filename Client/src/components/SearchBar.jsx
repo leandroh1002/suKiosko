6263d1e72caa73194b2f62486b323cac9e9c6805
leandroh1002 <leandroherrera1002@gmail.com>
2025-02-05 14:18:22 -0300

    add component searchbar

---
+++ Client/src/components/SearchBar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { allProduct, clear } from "../redux/actions";
+import { useDispatch } from "react-redux";
+
+export default function SearchBar(props) {
+
+  const {searchInput, setSearchInput} = props
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (searchInput.length != 0) {
+      const query = `&fullName=${searchInput}`
+      dispatch(allProduct(query))
+    }
+  };
+
+  const handleChange = (event) => {
+    setSearchInput(event.target.value);
+  }
+
+  const handleClear = () => {
+    setSearchInput("")
+    dispatch(clear())
+  }
+
+  const handleAll = () => {
+    dispatch(allProduct(""))
+  }
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.container_form} onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Buscar usuario por Nombre Completo"
+          onChange={handleChange}
+          value={searchInput} />
+        <button type="submit">Buscar</button>
+        <button type="button" onClick={()=>handleClear()}>Limpiar</button>
+        <button type="button" onClick={()=>handleAll()}>Todos</button>
+      </form>
+
+    </div>
+  );
+}
