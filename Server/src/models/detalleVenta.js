d1653284311ff0da93bab1d3fc026f7646e4341b
leandroh1002 <leandroherrera1002@gmail.com>
2025-05-29 22:59:05 -0300

    tabla detalle de ventas

---
+++ Server/src/models/detalleVenta.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require('sequelize');
+// const sequelize = require('../config/database'); // Asegúrate de configurar tu conexión a la base de datos
+
+module.exports = (sequelize) => {
+sequelize.define('DetalleVenta', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  venta_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'ventas', // Nombre de la tabla relacionada
+      key: 'id',
+    },
+  },
+  producto_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'productos', // Nombre de la tabla relacionada
+      key: 'id',
+    },
+  },
+  cantidad: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  precio_unitario: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  precio_compra: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  subtotal_compra: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  subtotal: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+}, {
+  tableName: 'detalle_venta',
+  timestamps: false, // No se necesitan created_at ni updated_at
+});
+};
