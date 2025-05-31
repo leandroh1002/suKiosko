6f574580c81beaf179693c5469db0b501b306552
leandroh1002 <leandroherrera1002@gmail.com>
2025-05-30 21:12:24 -0300

    componente de gestion

---
+++ Client/src/components/Gestionar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PATHROUTES from '../helpers/PathRoutes.helper'
+import { Link } from 'react-router-dom'
+import ControlDeVentas from './ControlDeVentas'
+import AdminStatistics from './AdminStatistics/AdminStatistics'
+
+function Gestionar() {
+
+  return (
+    <div className='grid min-h-dvh grid-rows-[auto_1fr_auto]'>
+      <h2>Gestionar</h2>
+      <div className='flex justify-around h-14'>
+        <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Revision de Stock</button>
+        <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Agregar Productos</button>
+        <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Control de Ventas</button>
+      </div>
+      <content>
+       <AdminStatistics /> 
+        {/* <ControlDeVentas /> */}
+      </content>
+      <footer>
+      <Link to={PATHROUTES.LANDING}><button className='bg-[#A64208] m-8 text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Volver</button></Link>
+      </footer>
+    </div>
+  )
+}
+
+export default Gestionar
