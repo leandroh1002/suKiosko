eb3c15136332f089b228d7becfd4416fcc4aa7a0
leandroh1002 <leandroherrera1002@gmail.com>
2025-07-02 20:58:29 -0300

    panel de gestion

--- Client/src/components/Gestionar.jsx
+++ Client/src/components/Gestionar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PATHROUTES from '../helpers/PathRoutes.helper'
 import { Link } from 'react-router-dom'
-import ControlDeVentas from './ControlDeVentas'
 import AdminStatistics from './AdminStatistics/AdminStatistics'
 
 function Gestionar() {
@@ -11,9 +10,12 @@ function Gestionar() {
       <h2>Gestionar</h2>
       <div className='flex justify-around h-14'>
         <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Revision de Stock</button>
+        <Link to={PATHROUTES.AGREGAR_PRODUCTOS}>
         <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Agregar Productos</button>
+        </Link>
         <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Control de Ventas</button>
       </div>
+
       <content>
        <AdminStatistics /> 
         {/* <ControlDeVentas /> */}
