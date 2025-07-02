095901b8dbbdace3461b8e40236f49caee8db53c
leandroh1002 <leandroherrera1002@gmail.com>
2025-07-01 23:01:27 -0300

    clientes modificado

--- Server/src/models/clientes.js
+++ Server/src/models/clientes.js
@@ -1,32 +1,34 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database'); // Asegúrate de configurar tu conexión a la base de datos
 
-const Cliente = sequelize.define('Cliente', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  nombre: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  telefono: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  direccion: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  dni: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true, // Para evitar duplicados
-  },
-}, {
-  tableName: 'clientes',
-  timestamps: false, // No se necesitan created_at ni updated_at
-});
-
-module.exports = Cliente;
+// Exportamos una función que define el modelo
+// Luego le inyectamos la conexión a Sequelize.
+module.exports = (sequelize) => {
+  // Definimos el modelo
+  sequelize.define('Cliente', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    telefono: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    direccion: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    dni: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true, // Para evitar duplicados
+    },
+  }, {
+    tableName: 'clientes',
+    timestamps: false, // No se necesitan created_at ni updated_at
+  });
+};
