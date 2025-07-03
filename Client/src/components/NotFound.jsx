718108f3567ed95d35e914141bb0fd5df25565d0
leandroh1002 <leandroherrera1002@gmail.com>
2025-07-02 23:17:01 -0300

    not found

--- Client/src/components/NotFound.jsx
+++ Client/src/components/NotFound.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import PATHROUTES from '../helpers/PathRoutes.helper'
 
 function NotFound() {
   return (
     <div>Error 404
 
-      <h4>hola</h4>
+      <h4>Pagina en construccion</h4>
+      <Link to={PATHROUTES.LANDING}><button>Home</button></Link>
     </div>
   )
 }
