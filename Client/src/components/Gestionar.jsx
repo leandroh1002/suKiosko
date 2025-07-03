ee66002175332292a4ace598209bc5d6c26a1823
leandroh1002 <leandroherrera1002@gmail.com>
2025-07-03 01:33:59 -0300

    gestionar componente

--- Client/src/components/Gestionar.jsx
+++ Client/src/components/Gestionar.jsx
@@ -9,11 +9,14 @@ function Gestionar() {
     <div className='grid min-h-dvh grid-rows-[auto_1fr_auto]'>
       <h2>Gestionar</h2>
       <div className='flex justify-around h-14'>
-        <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Revision de Stock</button>
+        <Link to={PATHROUTES.REVISIONSTOCK}>
+        <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Revision de Stock</button></Link>
         <Link to={PATHROUTES.AGREGAR_PRODUCTOS}>
         <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Agregar Productos</button>
         </Link>
+        <Link to={PATHROUTES.CONTROLDEVENTAS}>
         <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Control de Ventas</button>
+        </Link>
       </div>
 
       <content>
