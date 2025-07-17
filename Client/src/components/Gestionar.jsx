ff4b8826c2b28dd20c7250395273a2cbe165effe
leandroh1002 <leandroherrera1002@gmail.com>
2025-07-16 22:39:55 -0300

    gestion

--- Client/src/components/Gestionar.jsx
+++ Client/src/components/Gestionar.jsx
@@ -15,7 +15,10 @@ function Gestionar() {
         <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Agregar Productos</button>
         </Link>
         <Link to={PATHROUTES.CONTROLDEVENTAS}>
-        <button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Control de Ventas</button>
+        <button className='bg-[#a64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Control de Ventas</button>
+        </Link>
+        <Link to={PATHROUTES.RUBROS}>
+        <button className='bg-[#a64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Rubros</button>
         </Link>
       </div>
 
