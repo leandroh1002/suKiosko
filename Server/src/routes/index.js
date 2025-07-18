5c8b0707ca37f85eb797d43284df69ca63715405
leandroh1002 <leandroherrera1002@gmail.com>
2025-07-17 23:13:22 -0300

    index rutas

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -4,9 +4,15 @@ const router = Router();
 const productos = require("./productos.route.js");
 const ventas = require("./ventas.route.js");
 const rubros = require("./rubros.route.js");
+const graficos = require("./graficos.route.js");
+const gastos = require("./gastos.route.js");
+const unidades = require("./unidades.route.js");
 
+router.use(gastos);
+router.use(graficos);
 router.use(productos);
 router.use(ventas);
 router.use(rubros);
+router.use(unidades);
 
 module.exports = router;
