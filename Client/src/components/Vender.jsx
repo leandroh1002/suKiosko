dd941fdcebbbd0f15227ba40a1d7762ef70f94e9
leandroh1002 <leandroherrera1002@gmail.com>
2025-08-06 11:17:40 -0300

    vender componente

--- Client/src/components/Vender.jsx
+++ Client/src/components/Vender.jsx
@@ -11,7 +11,7 @@ function Vender() {
         <ListaProducto/>
         <DetalleVenta/>
         </div>
-        <Link to={PATHROUTES.LANDING}><button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Volver</button></Link>
+        <Link to={PATHROUTES.HOME}><button className='bg-[#A64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Volver</button></Link>
     </div>
   )
 }
