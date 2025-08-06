51a627267e2956d7e6f830d84b88b8ec42a4a166
leandroh1002 <leandroherrera1002@gmail.com>
2025-08-06 11:17:32 -0300

    componente gestionar

--- Client/src/components/Gestionar.jsx
+++ Client/src/components/Gestionar.jsx
@@ -20,6 +20,9 @@ function Gestionar() {
         <Link to={PATHROUTES.RUBROS}>
         <button className='bg-[#a64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Rubros</button>
         </Link>
+        <Link to={PATHROUTES.LISTAVENTAS}>
+        <button className='bg-[#a64208] text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Lista de Ventas</button>
+        </Link>
       </div>
 
       <content>
@@ -27,7 +30,7 @@ function Gestionar() {
         {/* <ControlDeVentas /> */}
       </content>
       <footer>
-      <Link to={PATHROUTES.LANDING}><button className='bg-[#A64208] m-8 text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Volver</button></Link>
+      <Link to={PATHROUTES.HOME}><button className='bg-[#A64208] m-8 text-white font-normal p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90'>Volver</button></Link>
       </footer>
     </div>
   )
