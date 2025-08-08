a844448167b1cb6d437b5f7a5413d051222842cf
leandroh1002 <leandroherrera1002@gmail.com>
2025-08-08 20:18:58 -0300

    dashboardview

--- Client/src/components/Views/DashboardView.jsx
+++ Client/src/components/Views/DashboardView.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Dashboard from '../Dashboard'
+import UploadExcel from '../UploadExcel/UploadExcel'
 
 function DashboardView() {
   return (
     <div className="flex flex-col min-h-[55vh]">
     <div className="flex-grow">
       <Dashboard />
+      <UploadExcel />
     </div>
   </div>
   )
