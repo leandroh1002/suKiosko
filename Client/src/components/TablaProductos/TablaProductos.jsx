0f89ceb8017bc66ec16ebc8eb5e993286499d281
leandroh1002 <leandroherrera1002@gmail.com>
2025-08-08 20:18:50 -0300

    tabla de productos

---
+++ Client/src/components/TablaProductos/TablaProductos.jsx
@@ -0,0 +1,128 @@
+
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { allProduct, updateProduct } from '../../redux/actions';
+import styles from './TablaProductos.module.scss';
+import PATHROUTES from '../../helpers/PathRoutes.helper';
+import { Link } from 'react-router-dom';
+
+const TablaProductos = () => {
+  const dispatch = useDispatch();
+  const products = useSelector(state => state.productForAdmin);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [editableData, setEditableData] = useState({});
+
+  useEffect(() => {
+    dispatch(allProduct(''));
+  }, [dispatch]);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleInputChange = (productId, field, value) => {
+    setEditableData(prev => ({
+      ...prev,
+      [productId]: {
+        ...prev[productId],
+        [field]: value,
+      },
+    }));
+  };
+
+  const handleSaveChanges = (productId) => {
+    const product = products.find(p => p.id === productId);
+    const editedData = editableData[productId] || {};
+
+    const newStock = editedData.nuevoStock !== undefined ? parseInt(product.stock) + parseInt(editedData.nuevoStock) : product.stock;
+    const newPrecioCompra = editedData.precioCompraNuevo !== undefined ? editedData.precioCompraNuevo : product.precio_compra;
+    const newPrecioVenta = editedData.precioVentaNuevo !== undefined ? editedData.precioVentaNuevo : product.redondeo;
+
+    const productData = {
+      ...product,
+      stock: newStock,
+      precio_compra: newPrecioCompra,
+      redondeo: newPrecioVenta,
+    };
+
+    dispatch(updateProduct(productId, productData));
+  };
+
+  const filteredProducts = products.filter(product => {
+    const searchTermLower = searchTerm.toLowerCase();
+    return (
+      product.nombre.toLowerCase().includes(searchTermLower) ||
+      product.codigo_barra?.toLowerCase().includes(searchTermLower)    );
+  });
+
+  return (
+    <div className={styles.container}>
+      <input
+        type="text"
+        placeholder="Buscar por nombre o código de barras"
+        value={searchTerm}
+        onChange={handleSearch}
+        className={styles.searchInput}
+      />
+      <Link to={PATHROUTES.REVISIONSTOCK} className='bg-[#a64208] text-white font-normal mb-7 p-2 rounded-lg cursor-pointer px-5 py-2 hover:bg-[#b45d2b] transition duration-75 transform hover:scale-105 active:bg-[#F2B138] active:scale-90 '>
+      <button className='mb-4' >Volver</button></Link>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Rubro</th>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Vencimiento</th>
+            <th>Código de Barras</th>
+            <th>Stock</th>
+            <th>Nuevo Stock</th>
+            <th>Precio Compra</th>
+            <th>Precio Compra Nuevo</th>
+            <th>Precio Venta</th>
+            <th>Precio Venta Nuevo</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredProducts.map(product => (
+            <tr key={product.id}>
+              <td>{product.Rubro ? product.Rubro.nombre : 'N/A'}</td>
+              <td>{product.nombre}</td>
+              <td>{product.descripcion}</td>
+              <td>{new Date(product.vencimiento).toLocaleDateString()}</td>
+              <td>{product.codigo_barra ? product.codigo_barra.padStart(13, '0') : 'N/A'}</td>
+              <td>{product.stock}</td>
+              <td>
+                <input
+                  type="number"
+                  onChange={(e) => handleInputChange(product.id, 'nuevoStock', e.target.value)}
+                />
+              </td>
+              <td>{product.precio_compra}</td>
+              <td>
+                <input
+                  type="number"
+                  onChange={(e) => handleInputChange(product.id, 'precioCompraNuevo', e.target.value)}
+                />
+              </td>
+              <td>{product.redondeo}</td>
+              <td>
+                <input
+                  type="number"
+                  onChange={(e) => handleInputChange(product.id, 'precioVentaNuevo', e.target.value)}
+                />
+              </td>
+              <td>
+                <button onClick={() => handleSaveChanges(product.id)}>
+                  Guardar Cambios
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TablaProductos;
